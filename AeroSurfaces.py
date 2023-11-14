6f1c5e83d3676b29297b0e34c9e9bdb07dcc5308
ben-groat <brgroat4@gmail.com>
2023-11-14 19:34:43 +0000

    added fin calcs

--- AeroSurfaces.py
+++ AeroSurfaces.py
@@ -1,10 +1,10 @@
 # General Imports, method is same as RocketPy coz it is neat code
 import numpy as np
+from function import Function
 
 
-class NoseCone():
+class NoseCone:
     def __init__(self, coneType, length, noseRadius, rocketRadius, shapeParameter):
-        
         self.type = coneType
         self.noseLength = length
         self.noseRadius = noseRadius
@@ -13,48 +13,49 @@ class NoseCone():
 
         # Aerodynamic Parameters
         # reference: https://offwegorocketry.com/userfiles/file/Nose%20Cone%20&%20Fin%20Optimization.pdf
-        self.k = None
+        self.Cna = 2  # doesn't need evaluating, it is 2 by default for a nosecone
+        self.Xn = None
         self.cp = None
         self.cl = None
         self.cd = None
 
     def addNose(self):
-    # Note the position of the nose is @ top of rocket coordinate system
+        # Note the position of the nose is @ top of rocket coordinate system
 
-        # if self.type == "VonKarman":    
-        #     self.k = 0.5
+        # if self.type == "VonKarman":
+        #     self.Xn = 0.5
         if self.type == "Haack":
-            self.k = self.length / 2 * (1 - (3 * self.shapeParameter / 8))
+            self.Xn = self.length / 2 * (1 - (3 * self.shapeParameter / 8))
         elif self.type == "Ogive":
             f = 1 / self.shapeParameter  # to be consistent with literature
             lam = np.sqrt(2 * f - 1)
             realOgiveXn = self.radius * ((f**2 - lam**2 / 3) * lam - f**2 * (f - 1) * np.arcsin(lam / f))
             # the actual ogive might be squashed, so we need to scale it
             realOgiveLength = np.sqrt((2 * lam + 1) * self.radius**2)
-            self.k = realOgiveXn * self.length / realOgiveLength
+            self.Xn = realOgiveXn * self.length / realOgiveLength
         else:
-            raise ValueError("Cannot find nose type?")
-        
-        self.geometricalParameters() # Evaluate geometrical parameters of nose
-        
+            raise ValueError("Cannot find nose type")
+
+        self.geometricalParameters()  # Evaluate geometrical parameters of nose
+
         self.evaluateCP()
         self.evaluateCL()
-        
+
         # Drawing for later broooooooooooooooooo
 
-    def geometricalParameters(self): # Finding rho, the radius ratio of nose cone
+    def geometricalParameters(self):  # Finding rho, the radius ratio of nose cone
         if self.noseRadius is None or self.rocketRadius is None:
             self.radiusRatio = 1
         else:
-            self.radiusRatio = self.noseRadius/self.rocketRadius
-        
+            self.radiusRatio = self.noseRadius / self.rocketRadius
+
     def evaluateCP(self):
-        self.cpx = self.k * self.noseLength
+        self.cpx = self.Xn
         self.cpy = 0
         self.cpz = 0
         self.cp = (self.cpx, self.cpy, self.cpz)
         return self.cp
-    
+
     def evaluateCL(self):
         # Calculate clalpha
         # clalpha is currently a constant, meaning it is independent of Mach
@@ -90,8 +91,69 @@ class NoseCone():
             "Cd",
         )
         return None
-    
-class Fins():
 
+
+class Fins:
+    def __init__(self, span, rootChord, tipChord, sweep, rootLocation, number, rocketRadius):
+        self.span = span
+        self.rootChord = rootChord
+        self.tipChord = tipChord
+        self.sweep = sweep
+        self.rootLocation = rootLocation
+        self.number = number
+        self.rocketRadius = rocketRadius
+
+        # Aerodynamic Parameters
+        # reference: https://offwegorocketry.com/userfiles/file/Nose%20Cone%20&%20Fin%20Optimization.pdf
+        self.Cna = None
+        self.Xn = None
+        self.cp = None
+        self.cl = None
+        self.cd = None
+
+    def addFins(self):
+        # Note the position of the nose is @ top of rocket coordinate system
+
+        l = np.sqrt(self.span**2 + (abs(self.rootChord - self.tipChord) + self.sweep) ** 2)
+        self.Cna = (
+            (self.number * self.span / (2 * self.rocketRadius)) ** 2
+            / (1 + np.sqrt(1 + (2 * l / (self.rootChord + self.tipChord)) ** 2))
+            * (1 + self.rocketRadius / (self.span + self.rocketRadius))
+        )
+        self.Xn = (
+            self.rootLocation
+            + (self.sweep * (self.rootChord + 2 * self.tipChord) / (3 * (self.rootChord + self.tipChord)))
+            + (self.rootChord + self.tipChord - (self.rootChord * self.tipChord) / (self.rootChord + self.tipChord))
+            / 6
+        )
+
+        self.geometricalParameters()  # Evaluate geometrical parameters of nose
+
+        self.evaluateCP()
+        self.evaluateCL()
+
+        # Drawing for later broooooooooooooooooo
+
+    def evaluateCP(self):
+        self.cpx = self.Xn
+        self.cpy = 0
+        self.cpz = 0
+        self.cp = (self.cpx, self.cpy, self.cpz)
+        return self.cp
+
+    def evaluateCL(self):
+        pass
+
+    def evaluateCD(self):
+        pass
+
+
+class BodyTube:
     pass
 
+
+class BoatTail:
+    pass
+
+
+#
